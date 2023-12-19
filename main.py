89c6aec735d7e44cf089b822a0e20b5f46e6d1bd
gaojing8500 <gaojing850063636@sina.com>
2023-12-19 23:32:36 +0800

    feat:新增hottrace 源信息

---
+++ main.py
@@ -0,0 +1,11 @@
+from arxiv_daily import get_daily_paper
+
+
+def test_get_daily_paper():
+    get_daily_paper()
+
+
+
+if __name__ == "__main__":
+    print("welcome hot trace")
+    test_get_daily_paper()
